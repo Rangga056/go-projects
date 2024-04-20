e403f01ad4d41206e7dcf5e5d34e770103f43d35
Rangga056 <muhammadrangga056@gmail.com>
2024-04-20 16:36:24 +0700

    first commit

---
+++ booking-app/main.go
@@ -0,0 +1,48 @@
+package main
+
+import "fmt"
+
+func main() {
+	var conferenceName string = "Go Conference"
+	const conferenceTickets uint = 50
+	var remainingTickets uint = 50
+	var bookings []string //This is a slice
+
+	fmt.Printf("Welcome to our %v application\n", conferenceName)
+	fmt.Printf("We have a total of %v tickets and %v still avaiable.\n", conferenceTickets, remainingTickets)
+	fmt.Println("Get your tickets here to attend")
+
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets uint //uint = postifive number
+	// ask user for their name
+	fmt.Println("Enter your first name: ")
+	fmt.Scan(&firstName)
+
+	fmt.Println("Enter your last name: ")
+	fmt.Scan(&lastName)
+
+	fmt.Println("Enter your email: ")
+	fmt.Scan(&email)
+
+	fmt.Println("Enter your number of tickets: ")
+	fmt.Scan(&userTickets)
+
+	if remainingTickets <= 0 || userTickets > remainingTickets {
+		fmt.Println("Sorry theres no more tickets avaiable")
+	} else {
+		remainingTickets = remainingTickets - userTickets
+		// bookings[0] = firstName + " " + lastName
+		bookings = append(bookings, firstName+" "+lastName)
+
+		fmt.Printf("The whole slice: %v\n", bookings)
+		fmt.Printf("The first value: %v\n", bookings[0])
+		fmt.Printf("Slice type: %T\n", bookings)
+		fmt.Printf("Slice length: %v\n", len(bookings))
+
+		fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a conirmation email at at %v\n", firstName, lastName, userTickets, email)
+	}
+
+	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+}
---
+++ go-bank/go.mod
@@ -0,0 +1,3 @@
+module github.com/Rangga056/go-bank
+
+go 1.22.1
